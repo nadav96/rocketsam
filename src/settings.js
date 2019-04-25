aabdae708dcdaa48ef3b499fc40c61f075649d74
Nadav Goldstein <nadav.goldstein96@gmail.com>
2019-04-25 21:57:02 +0300

    made the rocketsam cli fail gracefully if one of the rocketsam config fields is missing

--- src/settings.js
+++ src/settings.js
@@ -3,20 +3,27 @@
 const fs = require('fs-extra');
 const yaml = require('js-yaml')
 const path = require('path');
-
+const chalk = require('chalk')
 module.exports = parseSettings
 
 async function parseSettings() {
   const settings = await getRocketSamSettings()
   if (settings != undefined) {
-    return {
-      appDir: settings["appDir"].replace("$", process.cwd()),
-      buildDir: settings["buildDir"].replace("$", process.cwd()),
-      commonDir: settings["commonDir"].replace("$", process.cwd()),
-      resourcesDir: settings["resourcesDir"].replace("$", process.cwd()),
-      storageBucketName: settings["storageBucketName"].replace("$", process.cwd()),
-      stackName: settings["stackName"].replace("$", process.cwd()),
-      region: settings["region"]
+    try {
+      return {
+        appDir: settings["appDir"].replace("$", process.cwd()),
+        buildDir: settings["buildDir"].replace("$", process.cwd()),
+        commonDir: settings["commonDir"].replace("$", process.cwd()),
+        resourcesDir: settings["resourcesDir"].replace("$", process.cwd()),
+        storageBucketName: settings["storageBucketName"].replace("$", process.cwd()),
+        stackName: settings["stackName"].replace("$", process.cwd()),
+        region: settings["region"]
+      }
+    }
+    catch (e) {
+      console.log(chalk.red("One of the required feilds of the rocketsam.yaml file is missing, please refer to the readme for more info"));
+      
+      return undefined
     }
   } else {
     return undefined
